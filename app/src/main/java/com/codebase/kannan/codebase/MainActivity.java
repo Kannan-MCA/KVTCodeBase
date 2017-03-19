4c4485c7fad692e29a292ba3d5be051353ff4adf
kannan <kannan.p@rasiinfotech.com>
2017-03-19 14:12:10 +0530

    myfirstcommit

---
+++ app/src/main/java/com/codebase/kannan/codebase/MainActivity.java
@@ -0,0 +1,13 @@
+package com.codebase.kannan.codebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
